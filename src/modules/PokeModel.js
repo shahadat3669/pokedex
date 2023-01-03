1009cfaaeeb13c49521a70662b39ea591981b863
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-03 17:25:36 -0500

    Add PokeMOdel class. new file:   src/modules/PokeModel.js This class is for contain the info for each Pokemon.

---
+++ src/modules/PokeModel.js
@@ -0,0 +1,10 @@
+class PokeModel {
+  constructor(id, name, imgLink, types) {
+    this.id = id;
+    this.name = name;
+    this.imgLink = imgLink;
+    this.types = types;
+  }
+}
+
+export default PokeModel;
