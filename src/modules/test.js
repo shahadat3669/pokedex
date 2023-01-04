b2226c9d2e3fba605bfe1b1331ce0e49a4c3dda8
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-04 02:52:52 -0500

    Testing PokePresenter class.

--- src/modules/test.js
+++ src/modules/test.js
@@ -1,24 +1,12 @@
 import PokeApiService from './PokeApiService.js';
-import PokeView from './PokeView.js';
 
 async function test() {
   const cont = document.getElementById('poke-cards-cont');
   const pokeService = new PokeApiService();
   const pokeData = await pokeService.getSomePokemonData(125, 30);
-  let presenter;
-  let view;
 
   pokeData.forEach((element) => {
-    presenter = {
-      id: element.id,
-      imgLink: element.imgLink,
-      gen: 5,
-      name: element.name,
-      types: element.types,
-      likes: 3,
-    };
-    view = new PokeView(presenter);
-    cont.appendChild(view.ui);
+    cont.appendChild(element.view.ui);
   });
 }
 export default test;
