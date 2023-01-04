814eac1f9802e4a201f716012556e9d73de8cffb
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-04 01:42:45 -0500

    Testing the PokeView class.

--- src/modules/test.js
+++ src/modules/test.js
@@ -4,7 +4,7 @@ import PokeView from './PokeView.js';
 async function test() {
   const cont = document.getElementById('poke-cards-cont');
   const pokeService = new PokeApiService();
-  const pokeData = await pokeService.getSomePokemonData(25, 20);
+  const pokeData = await pokeService.getSomePokemonData(125, 30);
   let presenter;
   let view;
 
@@ -13,6 +13,9 @@ async function test() {
       id: element.id,
       imgLink: element.imgLink,
       gen: 5,
+      name: element.name,
+      types: element.types,
+      likes: 3,
     };
     view = new PokeView(presenter);
     cont.appendChild(view.ui);
