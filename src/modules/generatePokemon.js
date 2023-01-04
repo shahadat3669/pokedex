d55f6fc3edd7f44177bfe71f5d21e53bc2f2c457
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-04 23:55:56 +0600

    Call the api to get necessary datas.

--- src/modules/generatePokemon.js
+++ src/modules/generatePokemon.js
@@ -1,3 +1,9 @@
-const generatePokemon = () => {};
+import getPokemonData from './getPokemonData.js';
+
+const generatePokemon = async () => {
+  const result = await getPokemonData();
+  const { abilities, height, weight, id, name, stats, types, sprites } = result;
+  const img = sprites.other.dream_world.front_default;
+};
 
 export default generatePokemon;
