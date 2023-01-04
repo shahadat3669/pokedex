be6624131c510f5e498709b04a5f55875dc004b3
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-04 23:53:30 +0600

    Add functionality to generate background color.

--- src/modules/generatePokeBg.js
+++ src/modules/generatePokeBg.js
@@ -1,3 +1,66 @@
-const generateColor = () => {};
+const generateColor = async (type) => {
+  let color;
+  switch (type) {
+    case 'grass':
+      color = '#5fbd58';
+      break;
+    case 'bug':
+      color = '#92bc2c';
+      break;
+    case 'dark':
+      color = '#595761';
+      break;
+    case 'dragon':
+      color = '#0c69c8';
+      break;
+    case 'electric':
+      color = '#f2d94e';
+      break;
+    case 'fairy':
+      color = '#ee90e6';
+      break;
+    case 'fighting':
+      color = '#d3425f';
+      break;
+    case 'fire':
+      color = '#dc872f';
+      break;
+    case 'flying':
+      color = '#a1bbec';
+      break;
+    case 'ground':
+      color = '#da7c4d';
+      break;
+    case 'ghost':
+      color = '#5f6dbc';
+      break;
+    case 'psychic':
+      color = '#75d0c1';
+      break;
+    case 'steel':
+      color = '#5695a3';
+      break;
+    case 'water':
+      color = '#539ddf';
+      break;
+    case 'poison':
+      color = '#b763cf';
+      break;
+    case 'normal':
+      color = '#a0a29f';
+      break;
+    case 'rock':
+      color = '#a38c21';
+      break;
+    case 'ice':
+      color = '#75d0c1';
+      break;
+
+    default:
+      color = '#fff';
+      break;
+  }
+  return color;
+};
 
 export default generateColor;
