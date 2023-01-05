b361a3cd02357e3ab3a951afacb1ecf1a2b20229
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-05 21:31:54 +0600

    Add generateComments function to generate comment list elements.

---
+++ src/modules/generateComments.js
@@ -0,0 +1,22 @@
+import getCommentsData from './getPokemonComments.js';
+
+const generateComments = async (pokiId) => {
+  const result = await getCommentsData(pokiId);
+  let generateHtml = '';
+
+  if (!result.error && result.length > 0) {
+    await result.forEach(async (comment, id) => {
+      generateHtml += `<li class="comments__item" id=${id}>
+          <span class="comment__item-date">${comment.creation_date}</span>
+          <span class="comment__item-user">${comment.username}:</span>
+          <span class="comment__item-text">${comment.comment}</span>
+        </li>`;
+    });
+  } else {
+    generateHtml +=
+      '<li class="comments__item"><span class="comment__item-user">No comments.</span></li>';
+  }
+  return generateHtml;
+};
+
+export default generateComments;
