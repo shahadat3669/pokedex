bf4d5f947cb2d2ea64a1eac14083bbaeca9390e4
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-06 00:13:47 +0600

    Fix postComment function to send data.

--- src/modules/postComments.js
+++ src/modules/postComments.js
@@ -3,10 +3,11 @@ const postComment = async (data) => {
     'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/5KlkgNesq7R7nP80GFWz/comments';
   try {
     const response = await fetch(endpoint, {
-      method: 'post',
+      method: 'POST',
       headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
     });
-    return response.json();
+    return response.status;
   } catch (err) {
     throw new Error(
       `There was the following problem: ${err} while fetching ${endpoint}`,
