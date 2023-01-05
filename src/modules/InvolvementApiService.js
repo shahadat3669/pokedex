c59b7be077c7c9974c2527051fc5fad51c084c7e
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-05 14:25:26 -0500

    Add the post likes method and refactor to static methods.

--- src/modules/InvolvementApiService.js
+++ src/modules/InvolvementApiService.js
@@ -1,19 +1,40 @@
 class InvolvementApiService {
   #baseLikesUrl;
 
+  static #likesUrl;
+
+  static {
+    this.#likesUrl = new URL('/capstoneApi/apps/uVghFZUXyAONTeUqAIXl/likes',
+      'https://us-central1-involvement-api.cloudfunctions.net');
+  }
+
   constructor() {
     const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net';
     this.#baseLikesUrl = new URL('/capstoneApi/apps/uVghFZUXyAONTeUqAIXl/likes', baseUrl);
   }
 
-  getLikesById = async (pokeId) => {
+  static postLikeById = async (pokeId) => {
+    const response = await fetch(this.#likesUrl, {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: pokeId,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    return response.result;
+  }
+
+  static getLikesById = async (pokeId) => {
     const allLikes = await this.getAllLikes();
     const likes = allLikes.find((item) => item.item_id === pokeId);
 
     return (likes !== undefined) ? likes.likes : 0;
   }
 
-  getAllLikes = async () => fetch(this.#baseLikesUrl)
+  static getAllLikes = async () => fetch(this.#likesUrl)
     .then((response) => response.json());
 }
 export default InvolvementApiService;
