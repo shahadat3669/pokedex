edf4abc5e467598bf25334dd3c31684f65ddaf85
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-06 14:37:46 -0500

    Fix the tests.

--- __test__/AllPokesCounter.test.js
+++ __test__/AllPokesCounter.test.js
@@ -9,31 +9,29 @@ describe('AllPokesCounter', () => {
     document.body.innerHTML = '<div><div id="poke-cards-cont"></div><span id="poke-counter"></span></div>';
     const pokeCont = document.getElementById('poke-cards-cont');
     let htmlCards = '';
+
     for (let i = 0; i < 15; i += 1) {
       htmlCards += '<div class="poke-card"></div>';
     }
     pokeCont.innerHTML = htmlCards;
-    
-    const test = document.getElementById('poke-counter');
-
 
     // Action
     AllPokesCounter();
-    console.log(test.innerHTML);
+
     // Assert
-    expect(test.innerHTML).toStrictEqual('(15)');
+    const counter = document.getElementById('poke-counter');
+    expect(counter.innerHTML).toStrictEqual('(15)');
   });
 
   test('pokeCounter.innerText is equals to 0', () => {
     // Arrange
-    document.body.innerHTML = '<div><div id="poke-cards-cont"></div><span id="poke-counter"></span></div>';    
-    const test = document.getElementById('poke-counter');
+    document.body.innerHTML = '<div><div id="poke-cards-cont"></div><span id="poke-counter"></span></div>';
 
     // Action
     AllPokesCounter();
-    console.log(test.innerHTML);
 
     // Assert
-    expect(test.innerHTML).toStrictEqual('(0)');
+    const counter2 = document.getElementById('poke-counter');
+    expect(counter2.innerHTML).toStrictEqual('(0)');
   });
 });
